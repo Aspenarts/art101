b1c68d198e0d5bc72668536a11b2160eef8acaa6
Aspenarts <73726487+Aspenarts@users.noreply.github.com>
2022-10-24 19:39:04 -0700

    we did a lab
    yup

---
+++ lab7/js/lab.js
@@ -0,0 +1,74 @@
+/**
+ * Author:    Caitlin Morgan
+ * Created:   10.24.2022
+ *
+ *
+ **/
+ let favColor = prompt("Please enter your favorite color!", "purple");
+
+if (favColor != null) {
+  document.prompt("demo").innerHTML =
+  "The color " +favColor + "! that's a really cool color";
+}
+
+/*
+ * The code below was sounced from the author below
+ * Author: S Llawom Eydansele <wmodes@csumb.edu>
+ * Created: 19 September
+ * License: Public Domain
+ */
+
+// shuffleArray - take an array and shuffle the contents
+// Thanks to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = favColor.floor(favColor.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+// given a string, return string in Title Case
+// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function sortUserName(userName) {
+
+    return userName.toLowerCase().split("").sort().join("");
+}
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function randomizeName(userName) {
+    // convert userName string to an array
+    var nameArray = userName.toLowerCase().split("");
+    console.log("nameArray =", nameArray);
+    // shuffle array with our shuffle function
+    var shuffledArray = shuffleArray(nameArray);
+    console.log("shuffledArray =", shuffledArray);
+    var shuffledString = shuffledArray.join("");
+    // shift to Title Case (like a name)
+    var newName = toTitleCase(shuffledString);
+    // return array to a string
+    return newName;
+}
+
+function main() {
