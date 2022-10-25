40ee59c42fcf32ccded7a4b32391a4a1e6018fc1
Aspenarts <73726487+Aspenarts@users.noreply.github.com>
2022-10-24 19:54:39 -0700

    finished lab
    yup i hope

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,71 +4,23 @@
  *
  *
  **/
- let favColor = prompt("Please enter your favorite color!", "purple");
-
-if (favColor != null) {
-  document.prompt("demo").innerHTML =
-  "The color " +favColor + "! that's a really cool color";
+//sortUserName - a function that takes user input and sorts the letters
+function sortUserName() {
+  var userName = window.prompt("Hi! Tell me your favorite color so I can make you a new one.");
+  console.log("favoriteColor =", favoriteColor);
+  //split string to Arrays
+  var colorArray = favoriteColor.split('');
+  console.log("colorArray =", colorArray);
+  //sort the Arrays
+  var colorArraySort = colorArray.sort();
+  console.log("colorArraySort =", colorArraySort);
+  //Join array back to a string
+  var colorSorted = colorArraySort.join('');
+  console.log("colorSorted =", colorSorted);
+  //also could be a single line:
+  // favoriteColor.toLower().split("").sort().join("")
+  return colorSorted;
 }
-
-/*
- * The code below was sounced from the author below
- * Author: S Llawom Eydansele <wmodes@csumb.edu>
- * Created: 19 September
- * License: Public Domain
- */
-
-// shuffleArray - take an array and shuffle the contents
-// Thanks to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArray(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = favColor.floor(favColor.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
-
-// given a string, return string in Title Case
-// thanks to https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
-function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
-}
-
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
-function sortUserName(userName) {
-
-    return userName.toLowerCase().split("").sort().join("");
-}
-
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
-function randomizeName(userName) {
-    // convert userName string to an array
-    var nameArray = userName.toLowerCase().split("");
-    console.log("nameArray =", nameArray);
-    // shuffle array with our shuffle function
-    var shuffledArray = shuffleArray(nameArray);
-    console.log("shuffledArray =", shuffledArray);
-    var shuffledString = shuffledArray.join("");
-    // shift to Title Case (like a name)
-    var newName = toTitleCase(shuffledString);
-    // return array to a string
-    return newName;
-}
-
-function main() {
+//Output
+document.writeIn("Oooh, here's a new color for you: "),
+sortUserName(), "</br>");
