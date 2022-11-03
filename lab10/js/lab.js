a26b869c5590989e068c8fba646d3a398292f55d
Aspenarts <73726487+Aspenarts@users.noreply.github.com>
2022-11-02 21:05:44 -0700

    lab 9 and ten

---
+++ lab10/js/lab.js
@@ -0,0 +1,35 @@
+/**
+ * Author:    Caitlin Morgan
+ * Created:   10.26.2022
+ * Based off of assignment by Wess Modes
+ *
+ **/
+//sortUserName - a function that takes user input and sorts the letters
+buttonEl = document.getElementById("mybutton");
+console.log("button element:", buttonEl);
+
+buttonEl.addEventListener("sortUserName", function(){
+  var userName = inputEl.value;
+  var newName = sortUserName(userName);
+  outputEl = "<p id=name-results>" + newName + "</p>";
+  });
+
+function sortUserName() {
+  var favoriteColor = window.prompt("Hi! Tell me your favorite color so I can make you a new one.");
+  console.log("favoriteColor =", favoriteColor);
+  //split string to Arrays
+  var colorArray = favoriteColor.split('');
+  console.log("colorArray =", colorArray);
+  //sort the Arrays
+  var colorArraySort = colorArray.sort();
+  console.log("colorArraySort =", colorArraySort);
+  //Join array back to a string
+  var colorSorted = colorArraySort.join('');
+  console.log("colorSorted =", colorSorted);
+  //also could be a single line:
+  // favoriteColor.toLower().split("").sort().join("")
+  return colorSorted;
+}
+//Output
+document.writeln("Oooh, here's a new color for you: ", sortUserName(), "</br>");
+//not quite functional - but we plan on asking for some help for this lab at section on the 25th
---
+++ lab9/js/lab.js
@@ -0,0 +1,7 @@
+/**
+ * Author:    Caitlin Morgan
+ * Created:   10.26.2022
+ * Based off of assignment by Wess Modes
+ *
+ **/
+getElementById()
