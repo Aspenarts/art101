d52f8a90a6011194a0f2ce8121789fa0583912ff
Aspenarts <73726487+Aspenarts@users.noreply.github.com>
2022-12-03 17:40:17 -0800

    yup

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,19 +7,24 @@
 //sortUserName - a function that takes user input and sorts the letters
 var buttonEl = document.getElementById("mybutton");
 var outputEl = document.getElementById("output");
-console.log("button element:", buttonEl);
+var inputEl = document.getElementById("form-field");
+console.log("user input =", inputEl);
+
+
+//console.log("button element:", buttonEl);
 //add an input and add an output o7 ( append )
-buttonEl.addEventListener("sortUserName", function(){
+buttonEl.addEventListener('click', function(){
   var userName = inputEl.value;
   var newName = sortUserName(userName);
-  outputEl = "<p id=name-results>" + newName + "</p>";
-  });
+  console.log("sortef name =", newName);
 
-function sortUserName() {
-  var favoriteColor = window.prompt("Hi! Tell me your favorite color so I can make you a new one.");
-  console.log("favoriteColor =", favoriteColor);
+  outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
+});
+
+function sortUserName(input) {
+  var userName = String(input);
   //split string to Arrays
-  var colorArray = favoriteColor.split('');
+  var colorArray = userName.split('');
   console.log("colorArray =", colorArray);
   //sort the Arrays
   var colorArraySort = colorArray.sort();
---
+++ lab14/js/lab.js
@@ -0,0 +1,11 @@
+/**
+ * Author:    Caitlin Morgan
+ * Created:   10.26.2022
+ * Based off of assignment by Wess Modes
+ *
+ **/
+ $("#challenge").append("<button id='c-button'>Button</button>");
+
+ $("#problem").append("<button id='p-button'>Button</button>");
+
+ $("#result").append("<button id='r-button'>Button</button>");
